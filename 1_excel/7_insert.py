60219ec502841af56510f9db61c137bb71b41651
0206cho <68586044+0206cho@users.noreply.github.com>
2021-07-27 23:44:15 +0900

    엑셀 - 삽입

---
+++ 1_excel/7_insert.py
@@ -0,0 +1,16 @@
+from openpyxl import load_workbook
+wb = load_workbook("sample_5.xlsx")
+ws = wb.active
+
+# 8번째 줄이 비워짐
+# ws.insert_rows(8)
+
+# 8번쨰 줄 위치에 5줄 추가
+# ws.insert_rows(8,5)
+
+# 새로운 빈 열이 추가 - B열 비어짐
+# ws.insert_cols(2)
+
+# B번열부터 3열 추가
+# ws.insert_cols(2, 3)
+wb.save("sample_insert_cols.xlsx")
