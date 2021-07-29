4dfeddbb4aac0b86efe4d19a8592337ea93870b4
0206cho <68586044+0206cho@users.noreply.github.com>
2021-07-29 23:09:23 +0900

    데스크탑 UI - 이미지처리_대기

--- 2_ desktop/6_image_recognition.py
+++ 2_ desktop/6_image_recognition.py
@@ -37,3 +37,53 @@ import pyautogui
 # 3. 정확도 조정 - 픽셀 하나하나가 완전히 똑같지 않아도 몇 % 이상이면 똑같다고 인식하는거
 # check = pyautogui.locateOnScreen("check.png", confidence=0.9) # confidence : 기본값 0.99로 99%
 # pyautogui.moveTo(check)
+
+# 자동화 대상이 바로 보여지지 않는 경우
+# 1. 계속기다리기
+# file_menu_notpad = pyautogui.locateOnScreen("file_menu_notepad.png")
+# # 화면에 메모장이 가려져 있으므로 file_menu_notpad는 None가 들어감
+# # if file_menu_notpad:
+# #     pyautogui.click(file_menu_notpad)
+# # else:
+# #     print("발견 실패")
+# while file_menu_notpad is None: # 노트패드가 발견될까지 반복해서 찾음
+#     file_menu_notpad = pyautogui.locateOnScreen("file_menu_notepad.png")
+#     print("발견 실패")
+# pyautogui.click(file_menu_notpad)
+
+# 2. 일정 시간동안 기다리기(TimeOut)
+import time # 시간 사용하기 위해
+import sys # 프로그램 종료하기 위해
+
+# timeout = 10 #10초대기
+# start = time.time() # 시작 시간 설정
+# file_menu_notpad = None
+# while file_menu_notpad is None:
+#     file_menu_notpad = pyautogui.locateOnScreen("file_menu_notepad.png")
+#     end = time.time() # 종료 시간 설정
+#     if end - start > timeout: # 지정한 10초를 초과하면
+#         print("시간 종료")
+#         sys.exit()
+#
+# pyautogui.click(file_menu_notpad)
+
+# 위 코드함수로 정의
+def find_target(img_file, timeout=30):
+    start = time.time()
+    target = None
+    while target is None:
+        target = pyautogui.locateOnScreen(img_file)
+        end = time.time()
+        if end - start > timeout:
+            break
+    return target
+
+def my_click(img_file, timeout=30):
+    target = find_target(img_file, timeout)
+    if target:
+        pyautogui.click(target)
+    else:
+        print(f"[Timeout {timeout}s] Target not found ({img_file}). Terminate program.")
+        sys.exit()
+
+my_click("file_menu_notepad.png", 10)
